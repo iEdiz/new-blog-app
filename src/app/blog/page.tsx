a5c89eba6287c73e0720ee2193f1295e469a4372
Edijs Mezits <edijs.mezits@gmail.com>
2023-12-30 12:40:38 +0200

    Newblog

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./BlogPage.module.css";
+import PostCard from "@/components/PostCard/PostCard";
+
+const BlogPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
---
+++ src/components/PostCard/PostCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./PostCard.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            className={styles.img}
+          />
+        </div>
+        <span className={styles.date}>01.04.2023.</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et perspiciatis quaerat tenetur odit, eaque deleniti
+          aliquam expedita suscipit saepe nam?
+        </p>
+        <Link href="/blog/post">READ MORE</Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
